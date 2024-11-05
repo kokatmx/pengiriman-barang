819312914dbbbfeebbe8f5b83d2ec154a12c5ff0
kokatmx <umyuhm611@gmail.com>
2024-11-05 21:37:00 +0700

    updating view admin dashboard and add daisyui

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,5 +1,17 @@
-<h1>ini adalah halaman admin</h1>
-<form action="{{ route('logout') }}" method="post">
-    @csrf
-    <button type="submit">Logout</button>
-</form>
+<!DOCTYPE html>
+<html lang="en" data-theme="halloween">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+    <h1>ini adalah halaman admin</h1>
+    <form action="{{ route('logout') }}" method="post">
+        @csrf
+        <button type="submit">Logout</button>
+    </form>
+</body>
+</html>
